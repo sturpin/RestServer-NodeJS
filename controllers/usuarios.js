b315878a0fea91f94db3809dcf158b4fe16c31ed
sturpin <turpiniko@gmail.com>
2021-12-26 15:11:26 +0100

    First commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,55 @@
+const { response } = require('express');
+
+
+const usuariosGet = (req, res = response) => {
+
+    const { q, nombre = 'No name', apikey } = req.query;
+
+    res.json({
+        msg: 'get API - Controlador',
+        q,
+        nombre,
+        apikey
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - Controlador',
+        nombre, edad
+    })
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - Controlador',
+        id
+    })
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - Controlador'
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - Controlador'
+    })
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
